17face8b6208d1c1b9497fba435247a97db456ec
Zach Leslie <zach@puppetlabs.com>
2012-07-10 08:40:44 -0700

    Whitespace changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@ class askbot inherits askbot::params {
 
   # Resource Defaults
   Exec {
-    path => [ "/bin", "/usr/bin", "/sbin", "/usr/sbin" ],
+    path      => [ "/bin", "/usr/bin", "/sbin", "/usr/sbin" ],
     logoutput => on_failure,
   }
 
@@ -15,33 +15,33 @@ class askbot inherits askbot::params {
   }
 
   package { "askbot":
-    ensure => installed,
+    ensure   => installed,
     provider => $askbot_provider,
-    require => Package[$prereqs],
+    require  => Package[$prereqs],
   }
 
   file { "/var/log/askbot":
-    ensure => directory,
-    owner  => $web_user,
-    group  => $web_group,
+    ensure  => directory,
+    owner   => $web_user,
+    group   => $web_group,
     require => Package['askbot'],
   }
 
   file { "/var/log/askbot/askbot.log":
-    owner  => $web_user,
-    group  => $web_group,
-    mode => 0644,
+    owner   => $web_user,
+    group   => $web_group,
+    mode    => 0644,
     require => Package['askbot'],
   }
 
   # Allow users to upload files
   # This should be secured in some maner probably
   file { "upfiles":
-    path => "$askbot_home/upfiles",
-    ensure => directory,
-    owner => 0,
-    group => 0,
-    mode => 2755,
+    path    => "$askbot_home/upfiles",
+    ensure  => directory,
+    owner   => 0,
+    group   => 0,
+    mode    => 2755,
     require => Package['askbot'],
   }
 
@@ -66,30 +66,30 @@ class askbot inherits askbot::params {
   file { "/etc/askbot/sites/ask/config/__init__.py": 
      ensure => present,
      source => "puppet:///modules/askbot/setup_templates/__init__.py",
-     owner => 0,
-     group => 0,
+     owner  => 0,
+     group  => 0,
    }
 
   file { "/etc/askbot/sites/ask/config/urls.py": 
      ensure => present,
      source => "puppet:///modules/askbot/setup_templates/urls.py",
-     owner => 0,
-     group => 0,
+     owner  => 0,
+     group  => 0,
    }
 
   file { "/etc/askbot/sites/ask/config/manage.py":
      ensure => present,
      source => "puppet:///modules/askbot/setup_templates/manage.py",
-     owner => 0,
-     group => 0,
+     owner  => 0,
+     group  => 0,
    }
 
   file { "askbot_webconfig":
-     path => $askbot_webconfig,
-     ensure => present,
-     content => template('askbot/webserver_config.erb'),
-     require => File['/etc/askbot/sites/ask/config/settings.py'],
-     notify => Service['webserver']
+    path    => $askbot_webconfig,
+    ensure  => present,
+    content => template('askbot/webserver_config.erb'),
+    require => File['/etc/askbot/sites/ask/config/settings.py'],
+    notify  => Service['webserver'],
    }
 
   file { "/usr/sbin/askbot.wsgi":
@@ -113,12 +113,12 @@ class askbot inherits askbot::params {
   }
 
   file { "/etc/askbot/sites/ask/config/settings.py":
-    ensure => present,
+    ensure  => present,
     content => template('askbot/settings.erb'),
-    owner => $web_user,
-    group => $web_group,
-    mode => 0640,
+    owner   => $web_user,
+    group   => $web_group,
+    mode    => 0640,
     require => File["/etc/askbot/sites/ask/config"],
-    notify => Service['webserver'],
+    notify  => Service['webserver'],
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class askbot::params {
   #FIXME obviously these params are too specific for a generic module
 
   $askbot_site_name = "Puppet Labs"
-  $askbot_email = "stahnma@puppetlabs.com"
+  $askbot_email     = "stahnma@puppetlabs.com"
 
   # Must be informat of a zoneinfo file (e.g. 'America/Chicago' )
   $askbot_timezone = 'Zulu'
@@ -17,37 +17,37 @@ class askbot::params {
 
   # Mail Settings for askbot
 
-  $askbot_server_email = 'ask@puppetlabs.com'
-  $askbot_from_email = 'ask@puppetlabs.com'
-  $askbot_email_host_user = 'ask@puppetlabs.com'
-  $askbot_email_host_pass = 'XvQKPbDo '
+  $askbot_server_email         = 'ask@puppetlabs.com'
+  $askbot_from_email           = 'ask@puppetlabs.com'
+  $askbot_email_host_user      = 'ask@puppetlabs.com'
+  $askbot_email_host_pass      = 'XvQKPbDo '
   $askbot_email_subject_prefix = 'Puppet Labs Askbot'
-  $askbot_email_host = 'smtp.gmail.com'
-  $askbot_email_port = '587'
-  $askbot_email_tls = 'True'
+  $askbot_email_host           = 'smtp.gmail.com'
+  $askbot_email_port           = '587'
+  $askbot_email_tls            = 'True'
 
   # Generic Params
   case $::osfamily {
 
     'RedHat': {
       $py_location = "/usr/lib/python2.6/site-packages"
-      $prereqs = [  ]
-      $webserver = 'httpd'
-      $wsgi = 'mod_wsgi'
-      $web_user = 'apache'
-      $web_group = 'apache'
-      $askbot_provider = 'yum'
+      $prereqs          = [  ]
+      $webserver        = 'httpd'
+      $wsgi             = 'mod_wsgi'
+      $web_user         = 'apache'
+      $web_group        = 'apache'
+      $askbot_provider  = 'yum'
       $askbot_webconfig = "/etc/httpd/conf.d/askbot.conf"
     }
 
     'Debian': {
-       $py_location = "/usr/local/lib/python2.7/dist-packages"
-       $prereqs = [ 'python-pip', 'python-dev', 'python-psycopg2'  ]
-       $webserver = 'apache2'
-       $wsgi = "libapache2-mod-wsgi"
-       $web_user = 'www-data'
-       $web_group = 'www-data'
-       $askbot_provider = 'pip'
+       $py_location      = "/usr/local/lib/python2.7/dist-packages"
+       $prereqs          = [ 'python-pip', 'python-dev', 'python-psycopg2'  ]
+       $webserver        = 'apache2'
+       $wsgi             = "libapache2-mod-wsgi"
+       $web_user         = 'www-data'
+       $web_group        = 'www-data'
+       $askbot_provider  = 'pip'
        $askbot_webconfig = "/etc/apache2/conf.d/askbot.conf"
      } 
   }
